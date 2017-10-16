e17af5b68eb25c454dcef88cad39840fc9a09080
Karen G <kgraves0701@gmail.com>
2017-10-16 14:46:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {sade:"smooth operator"};
 function updatePlaylist(playlist, artist, song) {
   Object.assign({}, playlist, { artist: song })
 }
+
+function removeFromPlaylist(playlist,artist){
+  
+}
